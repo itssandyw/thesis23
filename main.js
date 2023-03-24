a61eff9fbd620b8112dd4056eb052fab8d647585
itssandyw <54820636+itssandyw@users.noreply.github.com>
2023-03-24 09:32:18 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,20 @@
 $(document).ready(function(){
 
-	var vid1 = document.getElementById("vid1");
-	vid1.play();
-   
-   	$("#section6 h1").hide();
-	$("#section6 h1").click(function() {
-		$("#section6 h2").show();
+	$('#comment').click(function(){
+		var input = $('#input').val();
+		$('.box').append(input + '<br>');
+		$('#input').val('');
+		$('.box-container').slidedown();
 	});
 
-	$(document).on("scroll", function() {
-		var pageTop = $(document).scrollTop();
-  		var pageBottom = pageTop + $(window).height();
-  		var tags = $(".tag");
+	$('#cancel').click(function(){
+		$('#input').val('');
+	});
 
-  		for (var i = 0; i < tags.length; i++) {
-    	var tag = tags[i];
+	$('#delete').click(function(){
+		$('.box').text('');
+		$('.box-container').slideup(); 
+	});	
 
-    	if ($(tag).position().top < pageBottom) {
-      	$(tag).addClass("visible");
-    	} else {
-      	$(tag).removeClass("visible");
-    }
-
-
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+};
 
